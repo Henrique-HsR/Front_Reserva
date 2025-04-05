2c1942b7cb3d2931ad949b32c064ca7c233b4cdd
Chrónos <chronos.net@outlook.com>
2025-04-05 12:56:25 -0300

    fixing connection back and front

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,34 +1,54 @@
+import { useEffect, useState, useRef } from "react";
 import "./style.css";
 import "../../index.css";
 import Trash from "../../assets/trash.svg";
+import api from "../../services/api";
 
-const moradores = [
-  {
-    id: "01",
-    name: "Henrique",
-    street: "D",
-    house: 11,
-    status: "Morador",
-  },
-  {
-    id: "02",
-    name: "Claudia",
-    street: "D",
-    house: 11,
-    status: "Proprietário",
-  },
-];
+//React Hooks --> useEffect, useState, useRef etc...
 
 function Home() {
+  const [moradores,setMoradores] = useState([])
+  const inputName = useRef()
+  const inputStreet = useRef()
+  const inputHouse = useRef()
+  const inputStatus = useRef()
+
+  async function getMoradores() {
+    const moradoresFromApi = await api.get('/moradores')
+
+    setMoradores(moradoresFromApi.data)
+  }
+
+  async function createMoradores() {
+    await api.post('/moradores', {
+      name: inputName.current.value,
+      street: inputStreet.current.value,
+      house: parseInt(inputHouse.current.value),
+      status: inputStatus.current.value
+    })
+
+    getMoradores()
+  }
+
+  async function deleteMoradores(id) {
+    await api.delete(`/moradores/${id}`)
+
+    getMoradores()
+  }
+
+  useEffect(() => {
+    getMoradores()
+  }, [])
+
   return (
     <div className="container">
       <form>
         <h1>Cadastro de Moradores</h1>
-        <input placeholder="Nome" name="name" type="text" />
-        <input placeholder="Rua" name="street" type="text" />
-        <input placeholder="Casa" name="house" type="number" />
-        <input placeholder="Status" name="status" type="text" />
-        <button type="button">Cadastrar</button>
+        <input placeholder="Nome" name="name" type="text" ref={inputName} />
+        <input placeholder="Rua" name="street" type="text" ref={inputStreet} />
+        <input placeholder="Casa" name="house" type="number" ref={inputHouse} />
+        <input placeholder="Status" name="status" type="text" ref={inputStatus} />
+        <button type="button"onClick={createMoradores}>Cadastrar</button>
       </form>
 
       {moradores.map((morador) => (
@@ -39,7 +59,7 @@ function Home() {
             <p>Casa: <span>{morador.house}</span></p>
             <p>Status: <span>{morador.status}</span></p>
           </div>
-          <button>
+          <button onClick={() => deleteMoradores(morador.id)}>
             <img src={Trash} alt="apagar" />
           </button>
         </div>
